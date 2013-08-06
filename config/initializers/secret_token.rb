ab907d6cec728466a2707d6b5dec3dba87bea16f
Dustin Phillips <dudoop@gmail.com>
2013-08-06 13:35:25 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'b427f58875ed3715e058077619443f0c03464f20c2de6d354c8dd41c0a887fd8a90a4712d28de2a94d2d9d89bc09d479c39ef2c8631d15456e86d00a6e7e5ff9'
